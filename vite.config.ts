c4cdaaff455c02b63b8f5babf4b46d078dedfd42
Pablo Henrique Castelo <pablohenriquec7@gmail.com>
2022-09-14 20:00:34 -0300

    alteração na base da vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pokedex-react-ts/'
+  base: 'https://pablohcs7.github.io/pokedex-react-ts/'
 })
