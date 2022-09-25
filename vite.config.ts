0d9c1b83d0b29f79e83ed3ae0d6207a59b1611f2
Pablo Henrique Castelo <pablohenriquec7@gmail.com>
2022-09-25 20:21:55 -0300

    teste deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'https://pablohcs7.github.io/pokedex-react-ts/'
+  plugins: [react()]
 })
